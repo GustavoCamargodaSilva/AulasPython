af87bce137d75f6c6cda0021d80224f12767a85b
Gustavo Camargo <gustavocamargodasilva@hotmail.com>
2022-08-13 12:35:08 -0300

    estrutura do codigo adivinhacao melhorada (while)

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,15 +1,25 @@
-print("Bem vindo ao jogo de Adivinhação!")
-#Declaração da variável
 numero_secreto = 43
-#Input para digitar o número/input sempre retorna string
-chute_str = input("Digite o seu número:")
-#Print para mostrar qual foi digitado
-print("Você digitou: ", chute_str)
-#variável para converter a string em número inteiro
-chute = int(chute_str)
-#IF para comparação == compara o tipo também
-if(numero_secreto == chute):
-    print("Você acertou!!")
-#Else para caso não tenha retorno igual
-else:
-    print("Você errou, tente novamente!!")
+total_de_tentativas = 3
+rodada = 1
+
+while (rodada <= total_de_tentativas):
+    print("Tentativa", rodada, "de", total_de_tentativas)
+    chute_str = input("Digite o seu número: ")
+    print("Você digitou: ", chute_str)
+    chute = int(chute_str)
+
+    acertou = numero_secreto == chute
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if (acertou):
+        print("Você acertou!")
+    else:
+        if (maior):
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        elif (menor):
+            print("Você errou! O seu chute foi menor que o número secreto.")
+
+    rodada = rodada + 1
+
+print("Fim do jogo")
