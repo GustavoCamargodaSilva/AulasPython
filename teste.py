f33041074551ae4fa34a5fc03778f39fd2c8eb17
Gustavo Camargo <gustavocamargodasilva@hotmail.com>
2022-08-13 11:55:40 -0300

    Validação de usuário com if, elif e else.

---
+++ teste.py
@@ -0,0 +1,13 @@
+
+usuario = input("Informe o usuário")
+
+if (usuario == "Flavio"):
+    print("Seja bem-vindo Flavio")
+elif (usuario == "Douglas"):
+    print("Seja bem-vindo Douglas")
+elif (usuario == "Nico"):
+    print("Seja bem-vindo Nico")
+else:
+    print("Usuário não identificado")
+
+
